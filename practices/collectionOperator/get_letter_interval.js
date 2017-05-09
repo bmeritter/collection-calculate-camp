d74736538066836120d88a6b8bbc70cdec70a168
bmeritter <bme_ritter@foxmail.com>
2017-05-09 16:26:12 +0800

    refactor get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const decrease = (number_a, number_b) => {
+const increase = (number_a, number_b) => {
   let result = [];
   for (let i = number_a; i <= number_b; ++i) {
     result.push(i);
@@ -8,16 +8,22 @@ const decrease = (number_a, number_b) => {
   return result;
 };
 
-function get_letter_interval(number_a, number_b) {
+const decrease = (number_a, number_b) => {
   let result = [];
-  if (number_a <= number_b) {
-    return decrease(number_a, number_b);
-  } else {
-    for (let i = number_a; i >= number_b; --i) {
-      result.push(i);
-    }
-    return result;
+  for (let i = number_a; i >= number_b; --i) {
+    result.push(i);
   }
+  return result;
+};
+
+
+function get_letter_interval(number_a, number_b) {
+  
+  if (number_a <= number_b) {
+    return increase(number_a, number_b);
+  }
+
+  return decrease(number_a, number_b);
 }
 
 module.exports = get_letter_interval;
