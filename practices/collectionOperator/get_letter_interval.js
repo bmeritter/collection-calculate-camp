8c824dc763904c694a5716add3468a5061ad8d56
bmeritter <bme_ritter@foxmail.com>
2017-05-09 16:24:01 +0800

    finish get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,23 @@
 'use strict';
 
+const decrease = (number_a, number_b) => {
+  let result = [];
+  for (let i = number_a; i <= number_b; ++i) {
+    result.push(i);
+  }
+  return result;
+};
+
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let result = [];
+  if (number_a <= number_b) {
+    return decrease(number_a, number_b);
+  } else {
+    for (let i = number_a; i >= number_b; --i) {
+      result.push(i);
+    }
+    return result;
+  }
 }
 
 module.exports = get_letter_interval;
