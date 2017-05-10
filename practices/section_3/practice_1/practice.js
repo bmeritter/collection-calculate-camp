9aea87488b2a42c4c9d5d4a66ef72949cabf4fc7
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:18:49 +0800

    finish section_3 practice_1

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,5 +1,10 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  return collection_a.map(item => {
+    if (object_b.value.indexOf(item.key) !== -1) {
+      return {key: item.key, count: item.count - 1};
+    }
+    return item;
+  });
 }
 
 module.exports = create_updated_collection;
