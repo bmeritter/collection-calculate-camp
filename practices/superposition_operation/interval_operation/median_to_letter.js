23b8e42cfc4885c6afc86bcabc5e0aff5c6938b0
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:55:53 +0800

    finish superposition_operation interval_operation median_to_letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function median_to_letter(collection) {
+  let rows = [''], cols = [];
+  for (let i = 97; i < 123; ++i) {
+    rows.push(String.fromCharCode(i));
+    cols.push(String.fromCharCode(i));
+  }
+  let mid = collection[parseInt(collection.length / 2)];
 
-  //在这里写入代码
+  return rows[parseInt((mid - 1) / 26)] + cols[parseInt((mid - 1) % 26)];
 }
 
 module.exports = median_to_letter;
