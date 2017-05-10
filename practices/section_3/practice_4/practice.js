cd841781a9a0bd259f1e38fcff69f7e6e8ef9873
bmeritter <bme_ritter@foxmail.com>
2017-05-10 22:21:42 +0800

    finish section_3 practice_4

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,29 @@
+const countKey = (collection_a) => {
+  let result = [];
+  collection_a.forEach(item => {
+    let tmp = item.split('-');
+    let exit = result.find(r => r.key === tmp[0]);
+    let count = parseInt(tmp[1]) || 1;
+    if (exit) {
+      exit.count += count;
+    } else {
+      result.push({key: item[0], count: count});
+    }
+  });
+  return result;
+};
+
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let result = countKey(collection_a);
+  return result.map((item) => {
+    let exit = object_b.value.find(v => item.key === v);
+    if (exit) {
+      item.count -= parseInt(item.count / 3);
+    }
+    return item;
+  });
+
+
 }
 
 module.exports = create_updated_collection;
