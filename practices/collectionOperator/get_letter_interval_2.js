0d7bcad01b5483b76ea4227fd915783a45dd8c74
bmeritter <bme_ritter@foxmail.com>
2017-05-10 13:07:57 +0800

    refactor get letter interval 2

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,32 +1,37 @@
 'use strict';
 
+const increase = (number_a, number_b, rows, cols) => {
+  let result = [];
+  for (let i = number_a - 1; i < number_b; ++i) {
+    let row = parseInt(i / 26);
+    let col = parseInt(i % 26);
+    result.push(rows[row] + cols[col]);
+  }
+  return result;
+};
+
+const decrease = (number_a, number_b, rows, cols) => {
+  let result = [];
+  for (let i = number_a - 1; i > number_b - 2; --i) {
+    let row = parseInt(i / 26);
+    let col = parseInt(i % 26);
+    result.push(rows[row] + cols[col]);
+  }
+  return result;
+};
+
 function get_letter_interval_2(number_a, number_b) {
-  let rows = [''], cols = [], result = [];
+  let rows = [''], cols = [];
   for (let i = 97; i < 123; ++i) {
     rows.push(String.fromCharCode(i));
     cols.push(String.fromCharCode(i));
   }
 
   if (number_a <= number_b) {
-    for (let i = number_a - 1; i < number_b; ++i) {
-      let row = parseInt(i / 26);
-      let col = parseInt(i % 26);
-      result.push(rows[row] + cols[col]);
-    }
-    return result;
-  } else {
-    for (let i = number_a - 1; i > number_b - 2; --i) {
-      let row = parseInt(i / 26);
-      let col = parseInt(i % 26);
-      // console.log(i, col)
-      // console.log(String.fromCharCode(col+96))
-      // console.log(rows[row] + cols[col])
-      result.push(rows[row] + cols[col]);
-    }
-    return result;
+    return increase(number_a, number_b, rows, cols);
   }
+  return decrease(number_a, number_b, rows, cols);
 }
 
-console.log(get_letter_interval_2(53, 20))
 module.exports = get_letter_interval_2;
 
