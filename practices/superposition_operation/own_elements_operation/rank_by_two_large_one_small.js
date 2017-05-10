ee3cc3aeb9c74835cfa5f677075b9ba32b6d440e
bmeritter <bme_ritter@foxmail.com>
2017-05-10 22:04:37 +0800

    finish superposition_operation own_elements_operation rank_by_two_large_one_small

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,21 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+
+const splicArray = (collection) => {
+  let result = [];
+  for (let i = 0; i < collection.length; i + 2) {
+    result.push(collection.splice(0, 3));
+  }
+  return result;
+};
+
+function rank_by_two_large_one_small(collection) {
+  let result = splicArray(collection.sort((a, b) => a - b));
+  for (let i = 0; i < result.length; ++i) {
+    if (result[i].length === 3) {
+      let first = result[i].shift();
+      result[i].push(first);
+    }
+  }
+  return result.reduce((a, b) => a.concat(b), []);
 }
 module.exports = rank_by_two_large_one_small;
