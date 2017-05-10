33aad62440a0fb3662d0395b526dc1fa110aafb6
bmeritter <bme_ritter@foxmail.com>
2017-05-10 15:33:43 +0800

    finish section_2 practice_1

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,14 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  collection.forEach(item => {
+    let exit = result.find(r => r.key === item);
+    if (exit) {
+      exit.count++;
+    } else {
+      result.push({key: item, count: 1});
+    }
+  });
+  return result;
 }
 
 module.exports = count_same_elements;
