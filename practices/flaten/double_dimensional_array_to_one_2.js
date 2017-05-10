59fa352d87461ec21a0581912eec418bf62f6f0d
bmeritter <bme_ritter@foxmail.com>
2017-05-10 13:56:16 +0800

    double dimensional array to one 2

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -2,7 +2,14 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+  let result = [];
+  collection.reduce((a, b) => a.concat(b), []).forEach(item => {
+    let exit = result.find(r => r === item);
+    if (!exit) {
+      result.push(item);
+    }
+  });
+  return result;
 }
 
 module.exports = double_to_one;
