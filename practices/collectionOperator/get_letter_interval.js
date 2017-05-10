894c74a55d8c820c32bae90245c6c163b1a1f69e
bmeritter <bme_ritter@foxmail.com>
2017-05-10 12:13:35 +0800

    refactor get letter interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,18 +1,26 @@
 'use strict';
 
-function get_letter_interval(number_a, number_b) {
+const increase = (number_a, number_b) => {
   let result = [];
-  if (number_a <= number_b) {
-    for (let i = number_a + 96; i <= number_b + 96; ++i) {
-      result.push(String.fromCharCode(i));
-    }
-    return result;
-  } else {
-    for (let i = number_a + 96; i >= number_b + 96; --i) {
-      result.push(String.fromCharCode(i));
-    }
-    return result;
+  for (let i = number_a + 96; i <= number_b + 96; ++i) {
+    result.push(String.fromCharCode(i));
   }
+  return result;
+};
+
+const decrease = (number_a, number_b) => {
+  let result = [];
+  for (let i = number_a + 96; i >= number_b + 96; --i) {
+    result.push(String.fromCharCode(i));
+  }
+  return result;
+};
+
+function get_letter_interval(number_a, number_b) {
+  if (number_a <= number_b) {
+    return increase(number_a, number_b);
+  }
+  return decrease(number_a, number_b);
 }
 
 module.exports = get_letter_interval;
