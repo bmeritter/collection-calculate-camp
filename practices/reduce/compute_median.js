4607e02a812e781e6bbe6e7147e968b03188c8aa
bmeritter <bme_ritter@foxmail.com>
2017-05-10 21:35:53 +0800

    finish practices reduce compute_median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  collection = collection.sort((a, b) => a - b);
+  if (collection.length % 2 === 0) {
+    return (collection[parseInt(collection.length / 2) - 1] + collection[parseInt(collection.length / 2)] ) / 2;
+  } else {
+    return collection[parseInt(collection.length / 2)];
+  }
 }
 
 module.exports = compute_median;
