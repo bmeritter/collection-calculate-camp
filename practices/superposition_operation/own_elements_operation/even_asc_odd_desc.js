6d0a25ded7379318a749791741ca052579a999d8
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:58:57 +0800

    finish superposition_operation own_elements_operation even_asc_odd_desc

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,6 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
+var even_asc_odd_desc = function (collection) {
+  return collection.filter(item => item % 2 === 0).sort((a, b) => a - b).concat(collection.filter(item => item % 2 !== 0).sort((a, b) => b - a));
 
 };
 module.exports = even_asc_odd_desc;
