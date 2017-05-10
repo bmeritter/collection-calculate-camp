ec25aa2e4d915f1dff0b866361e3e329e0f77e3d
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:38:36 +0800

    finish superposition_operation collection_operation letter_mapping_2

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  return String.fromCharCode(Math.ceil(collection.reduce((a, b) => a + b) / collection.length) + 96);
 }
 
 module.exports = average_to_letter;
