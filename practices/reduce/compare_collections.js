b59a1307f8018009ea18d02d59e8d7c86a3b1044
bmeritter <bme_ritter@foxmail.com>
2017-05-10 14:16:22 +0800

    finish compare collections

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  for (let i in collection_a) {
+    if (collection_a[i] !== collection_b[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = compare_collections;
