ea4b77462c98fcee13642ae45fc55f43578b88cf
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:35:14 +0800

    finish superposition_operation collection_operation practice_2

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  return collection.filter(item => item % 2 !== 0).map(item => item * 3 + 2);
 }
 
 module.exports = hybrid_operation_to_uneven;
