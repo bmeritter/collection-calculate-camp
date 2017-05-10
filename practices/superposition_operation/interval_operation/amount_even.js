bfdc2f796a2ecafe872bc1891353b556de75da95
bmeritter <bme_ritter@foxmail.com>
2017-05-10 20:41:23 +0800

    finish superposition_operation interbal_operation amount_even

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function amount_even(collection) {
-
-  //在这里写入代码
+  return collection.filter(item => item % 2 === 0).reduce((a, b) => a + b, 0);
 }
 
 module.exports = amount_even;
