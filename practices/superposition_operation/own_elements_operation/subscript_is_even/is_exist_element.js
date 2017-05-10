3191769ca000f0d7eac2893cfb342f2b1d81671c
bmeritter <bme_ritter@foxmail.com>
2017-05-10 21:09:06 +0800

    finish superposition_operation own_elements_operation subscript_is_even is_exist_element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,10 @@
 'use strict';
-var is_exist_element = function(collection,element){
+var is_exist_element = function (collection, element) {
+  let index = collection.indexOf((element));
+  if (index % 2 !== 0) {
+    return false;
+  }
+  return true;
 
 };
 module.exports = is_exist_element;
