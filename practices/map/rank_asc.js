6cc454658700b0e6f2468da501392a8c2c916ceb
bmeritter <bme_ritter@foxmail.com>
2017-05-10 13:47:04 +0800

    finish rank asc

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,6 @@
 'use strict';
-var rank_asc = function(collection){
-  return [6,5,4,3,2];
+var rank_asc = function (collection) {
+  return collection.sort((a, b) => b-a);
 };
 
 module.exports = rank_asc;
