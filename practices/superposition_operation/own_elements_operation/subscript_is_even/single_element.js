76e159e59e226a57a1fcd1456bae309ca901391c
bmeritter <bme_ritter@foxmail.com>
2017-05-10 21:19:11 +0800

    finish superposition_operation own_elements_operation subscript_is_even single_element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,14 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function (collection) {
+  collection = collection.filter((item, index) => (index + 1) % 2 === 0);
+  let result = [];
+  collection.forEach(item => {
+    if (result.indexOf(item) !== -1) {
+      result.splice(result.indexOf(item), 1);
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
 };
 module.exports = single_element;
