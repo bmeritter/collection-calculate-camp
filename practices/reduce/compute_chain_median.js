254f977b4f0f2ddd7afb312e4a83b748b89ddb74
bmeritter <bme_ritter@foxmail.com>
2017-05-10 14:39:11 +0800

    finish compute chain median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  collection = collection.split('->').sort((a, b) => a - b).map(item => parseInt(item));
+  if (collection.length % 2 !== 0) {
+    return collection[parseInt(collection.length / 2)];
+  }
+  let len = collection.length / 2;
+  let mid = parseInt((len + len + 1) / 2);
+  return (collection[mid - 1] + collection[mid]) / 2;
 }
 
 module.exports = compute_chain_median;
