4e42d42773fb29072ebe077f11cc607d8cb192a5
bmeritter <bme_ritter@foxmail.com>
2017-05-10 12:10:18 +0800

    finish get letter interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-
-
+  let result = [];
+  if (number_a <= number_b) {
+    for (let i = number_a + 96; i <= number_b + 96; ++i) {
+      result.push(String.fromCharCode(i));
+    }
+    return result;
+  } else {
+    for (let i = number_a + 96; i >= number_b + 96; --i) {
+      result.push(String.fromCharCode(i));
+    }
+    return result;
+  }
 }
 
 module.exports = get_letter_interval;
