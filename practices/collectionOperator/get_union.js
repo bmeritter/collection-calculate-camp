0bf3bbed54098e9e936686902ee1de69a57b1107
bmeritter <bme_ritter@foxmail.com>
2017-05-10 11:58:22 +0800

    finish get union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  let result = collection_a;
+  collection_b.forEach(b => {
+    let exit = result.find(item => item === b);
+    if (!exit) {
+      result.push(b);
+    }
+  });
+  return result;
+
 }
 
 module.exports = get_union;
