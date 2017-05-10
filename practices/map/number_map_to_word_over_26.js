dbbac3ca17a83fa932db99ceee225c0135837758
bmeritter <bme_ritter@foxmail.com>
2017-05-10 14:03:27 +0800

    number map to word over 26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,15 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word_over_26 = function (collection) {
+  let cols = [], rows = [''];
+  for (let i = 97; i < 123; ++i) {
+    rows.push(String.fromCharCode(i));
+    cols.push(String.fromCharCode(i));
+  }
+  return collection.map(item => {
+    let row = parseInt((item - 1) / 26);
+    let col = parseInt((item - 1) % 26);
+    return (rows[row] + cols[col]);
+  });
 };
 
 module.exports = number_map_to_word_over_26;
