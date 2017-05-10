acd6490afe4d357375c27c14a6e344651a7e1dfd
bmeritter <bme_ritter@foxmail.com>
2017-05-10 13:21:49 +0800

    finish grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  let result = {};
+  collection.filter(item => {
+    if (result[item]) {
+      result[item]++;
+    } else {
+      result[item] = 1;
+    }
+  });
+  return result;
 }
 
 module.exports = grouping_count;
