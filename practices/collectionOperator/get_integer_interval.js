f61fc4842953ae69eaab1cb4a895324a66f41a67
bmeritter <bme_ritter@foxmail.com>
2017-05-10 12:02:17 +0800

    find true src for get integer interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,29 @@
 'use strict';
 
+const increase = (number_a, number_b) => {
+  let result = [];
+  for (let i = number_a; i <= number_b; ++i) {
+    result.push(i);
+  }
+  return result;
+};
+
+const decrease = (number_a, number_b) => {
+  let result = [];
+  for (let i = number_a; i >= number_b; --i) {
+    result.push(i);
+  }
+  return result;
+};
+
+
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+
+  if (number_a <= number_b) {
+    return increase(number_a, number_b);
+  }
+
+  return decrease(number_a, number_b);
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,29 +1,8 @@
 'use strict';
 
-const increase = (number_a, number_b) => {
-  let result = [];
-  for (let i = number_a; i <= number_b; ++i) {
-    result.push(i);
-  }
-  return result;
-};
-
-const decrease = (number_a, number_b) => {
-  let result = [];
-  for (let i = number_a; i >= number_b; --i) {
-    result.push(i);
-  }
-  return result;
-};
-
-
 function get_letter_interval(number_a, number_b) {
 
-  if (number_a <= number_b) {
-    return increase(number_a, number_b);
-  }
 
-  return decrease(number_a, number_b);
 }
 
 module.exports = get_letter_interval;
