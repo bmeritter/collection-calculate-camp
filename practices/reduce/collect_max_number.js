278d8666306daba614bc063e8843c561f691cf27
bmeritter <bme_ritter@foxmail.com>
2017-05-10 14:09:56 +0800

    finish collect max number

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  return collection.reduce((a, b) => a > b ? a : b, collection[0]);
 }
 
 module.exports = collect_max_number;
