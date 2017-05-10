9c8d695ba3943348c4b32abbcc20685ae5f61b95
bmeritter <bme_ritter@foxmail.com>
2017-05-10 15:44:56 +0800

    finish section_2 practice_2

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,16 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  collection.forEach(item => {
+    let tmp = item.split('-');
+    let exit = result.find(r => r.key === tmp[0]);
+    let count = parseInt(tmp[1]) || 1;
+    if (exit) {
+      exit.count += count;
+    } else {
+      result.push({key: item[0], count: count});
+    }
+  });
+  return result;
 }
 
 module.exports = count_same_elements;
