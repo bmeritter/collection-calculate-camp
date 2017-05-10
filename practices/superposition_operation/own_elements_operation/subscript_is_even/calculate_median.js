2b0a9506b7f251632268395ab0e81cf36f81e480
bmeritter <bme_ritter@foxmail.com>
2017-05-10 21:33:09 +0800

    finish superposition_operation own_elements_operation subscript_is_even calculate_median

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,11 @@
 'use strict';
-var calculate_median = function(collection){
-
+var calculate_median = function (collection) {
+  collection = collection.filter(item => item % 2 === 0);
+  if (collection.length % 2 === 0) {
+    return (collection[parseInt(collection.length / 2) - 1] + collection[parseInt(collection.length / 2)] ) / 2;
+  } else {
+    return collection[parseInt(collection.length / 2)];
+  }
 };
+
 module.exports = calculate_median;
