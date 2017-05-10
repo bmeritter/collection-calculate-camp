262c8c74ec4d17e0ede720b510efce735c41a0dd
bmeritter <bme_ritter@foxmail.com>
2017-05-10 21:05:31 +0800

    finish superposition_operation own_elements_operation subscript_is_even calculate_average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,7 @@
 'use strict';
-var calculate_average = function(collection){
+var calculate_average = function (collection) {
+  let even = collection.filter(item => item % 2 === 0);
+  return even.reduce((a, b) => a + b, 0) / even.length;
 
 };
 module.exports = calculate_average;
