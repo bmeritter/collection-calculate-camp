b1a7b5f084b2598d7ff065b8dd14e8c308e357c5
bmeritter <bme_ritter@foxmail.com>
2017-05-10 15:22:49 +0800

    finish section_3 practice_3

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,9 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  return collection_a.filter(item => {
+    if (object_b.value.indexOf(item) !== -1) {
+      return item;
+    }
+  });
 }
 
 module.exports = collect_same_elements;
